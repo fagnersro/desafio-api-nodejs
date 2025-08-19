6f80d7f8f61bc35502eed0d765996c0c073d996f
Fagner Henrique <fagnersro.dev@gmail.com>
2025-08-19 15:04:26 -0300

    Aula 01

---
+++ server.ts
@@ -0,0 +1,65 @@
+const fastify = require('fastify')
+const crypto = require('crypto')
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+})
+
+const courses = [
+  {id: '1', title: 'Curso de Node.js'},
+  {id: '2', title: 'Curso de React'},
+  {id: '3', title: 'Curso de React Native'},
+]
+
+server.get('/courses', () => {
+  return { courses }
+})
+
+server.get('/courses/:id', (request, reply) => {
+  type Params = {
+    id: String
+  }
+
+  const params = request.params as Params
+  const courseId = params.id
+
+
+  const course = courses.find(course => course.id === courseId)
+
+  if (course) {
+    return { course }
+  }
+
+  return reply.status(404).send()
+})
+
+server.post('/courses', (request, reply) => {
+  type Body = {
+    title: string
+  }
+  
+  const coursesId = crypto.randomUUID()
+
+  const body = request.body as Body
+  const courseTitle = body.title
+
+  if (!courseTitle) {
+    return reply.status(400).send({ message: 'Título obrigatório.' })
+  }
+
+  courses.push({  id: coursesId , title: courseTitle })
+
+  return reply.status(201).send({ coursesId })
+})
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
